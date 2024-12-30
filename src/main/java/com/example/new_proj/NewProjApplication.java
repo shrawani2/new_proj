2a908ae9e02766d23856eec22d168a0f0f98df14
Shrawani <shrawani@gmail.com>
2024-12-30 20:13:43 +0530

    new project

---
+++ src/main/java/com/example/new_proj/NewProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_proj/NewProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
